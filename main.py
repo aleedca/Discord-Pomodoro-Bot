fc261a9094c151ee8691d95fd937d733e0534ef1
Jeel Patel <jeelpatel27602@gmail.com>
2022-07-26 04:43:29 +0530

    Token Set Up Done, Bot is live

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 import discord
 
+TOKEN = 'MTAwMTI1Mjg5MTI3NDc4ODk0NQ.GfotIK.uyk4RlfVC3rPe1rLi9xVevezuUeFaTv3GAwwJU'
+
 client = discord.Client()
 
-client.run('TOKEN HERE')
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    username = str(message.author).split("#")[0]
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    print(f'{username} : {user_message}  ({channel})')
+
+
+    if message.author == client.user:
+        return
+
+    if message.channel.name == "wanna-talk":
+        if user_message.lower() == "hello":
+            await message.channel.send(f'Hello {username}!')
+            return
+
+
+client.run(TOKEN)
